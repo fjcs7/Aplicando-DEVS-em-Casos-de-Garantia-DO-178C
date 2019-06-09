0fb4f4477a18e1bf7be13b529e630b00a18f0315
Gislainy Crisostomo Velaso <gislainycrisostomo@gmail.com>
2019-06-09 15:17:00 -0300

    Tentativa de ler o arquivo e armazenar numa lista na memória

--- ConformingTestDO-178C 6.3.1.a/src/Models/utils/ExampleArchiveGenerator.java
+++ ConformingTestDO-178C 6.3.1.a/src/Models/utils/ExampleArchiveGenerator.java
@@ -1,5 +1,6 @@
 package Models.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.print.attribute.HashAttributeSet;
@@ -17,22 +18,24 @@ public class ExampleArchiveGenerator {
 	public static void TesteArchSample(){
 		ReadFiles archive = new ReadFiles("PilotCommands.txt");
 		archive.ReadFile();
-		List<String> listText = new List<String>();
+		List<String> listText = new ArrayList<String>();
 		
 		CmdJoystick cmdJoy = new CmdJoystick();
 		while(archive.hasNextRow()){
 			if(archive.hasNextRow()){
 				listText.add(archive.getNextRow());
-				
 			}
-			
 		}
 		
-		for(Object item : listText){
-			
-		}
-		//cmdJoy = CmdJoystick.parseStringToCmdJoystick(archive.getNextRow());
+		for(String item : listText){
+			//System.out.println(item);
+			cmdJoy = CmdJoystick.parseStringToCmdJoystick(item);
 			System.out.println(cmdJoy);
+		}
+		System.out.println(listText.get(5));
+		/*//cmdJoy = CmdJoystick.parseStringToCmdJoystick(archive.getNextRow());
+			System.out.println(cmdJoy);
+			*/
 	}
 	
 	public static void GererateArchSample(){
