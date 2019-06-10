d63f500b1c64387e69c811aeaabaeca2911fcdee
Fernando Junio Cunha e Sousa <fjcs7@hotmail.com>
2019-06-10 13:27:43 -0300

    Bugs fixies.

--- ConformingTestDO-178C 6.3.1.a/src/Models/java/Wings.java
+++ ConformingTestDO-178C 6.3.1.a/src/Models/java/Wings.java
@@ -1,320 +1,45 @@
-/* Do not remove or modify this comment!  It is required for file identification!
-DNL
-platform:/resource/ConformingTestDO-178C%206.3.1.a/src/Models/dnl/Wings.dnl
--2053973754
- Do not remove or modify this comment!  It is required for file identification! */
 package Models.java;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-
-import java.io.File;
-import java.io.Serializable;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import java.util.ArrayList;
-
-import com.ms4systems.devs.core.message.Message;
-import com.ms4systems.devs.core.message.MessageBag;
-import com.ms4systems.devs.core.message.Port;
-import com.ms4systems.devs.core.message.impl.MessageBagImpl;
 import com.ms4systems.devs.core.model.impl.AtomicModelImpl;
-import com.ms4systems.devs.core.simulation.Simulation;
-import com.ms4systems.devs.core.simulation.Simulator;
-import com.ms4systems.devs.core.simulation.impl.SimulationImpl;
+import com.ms4systems.devs.core.message.Port;
+import java.io.Serializable;
 import com.ms4systems.devs.extensions.PhaseBased;
+import com.ms4systems.devs.extensions.ProvidesTooltip;
 import com.ms4systems.devs.extensions.StateVariableBased;
-import com.ms4systems.devs.helpers.impl.SimulationOptionsImpl;
-import com.ms4systems.devs.simviewer.standalone.SimViewer;
 
-@SuppressWarnings("unused")
-public class Wings extends AtomicModelImpl implements PhaseBased,
-    StateVariableBased {
-    private static final long serialVersionUID = 1L;
+public class Wings extends AtomicModelImpl implements PhaseBased, StateVariableBased, ProvidesTooltip { 
+	private static final long serialVersionUID = 1L;
 
-    // Declare state variables
-    private PropertyChangeSupport propertyChangeSupport =
-        new PropertyChangeSupport(this);
-    String phase = "waitforAngleLeft";
-    String previousPhase = null;
-    Double sigma = Double.POSITIVE_INFINITY;
-    Double previousSigma = Double.NaN;
-
-    // End state variables
-
-    // Input ports
-    //ID:INP:0
-    public final Port<Serializable> inAngleRight =
-        addInputPort("inAngleRight", Serializable.class);
-
-    //ENDID
-    //ID:INP:1
-    public final Port<Serializable> inAngleLeft =
-        addInputPort("inAngleLeft", Serializable.class);
-
-    //ENDID
-    // End input ports
-
-    // Output ports
-    //ID:OUTP:0
-    public final Port<Serializable> outYawAngleRight =
-        addOutputPort("outYawAngleRight", Serializable.class);
-
-    //ENDID
-    //ID:OUTP:1
-    public final Port<Serializable> outYawAngleLeft =
-        addOutputPort("outYawAngleLeft", Serializable.class);
-
-    //ENDID
-    // End output ports
-    protected SimulationOptionsImpl options = new SimulationOptionsImpl();
-    protected double currentTime;
-
-    // This variable is just here so we can use @SuppressWarnings("unused")
-    private final int unusedIntVariableForWarnings = 0;
-
-    public Wings() {
-        this("Wings");
-    }
-
-    public Wings(String name) {
-        this(name, null);
-    }
-
-    public Wings(String name, Simulator simulator) {
-        super(name, simulator);
-    }
-
-    public void initialize() {
-        super.initialize();
-
-        currentTime = 0;
-
-        passivateIn("waitforAngleLeft");
-
-    }
-
-    @Override
-    public void internalTransition() {
-        currentTime += sigma;
-
-        if (phaseIs("sendYawAngleLeft")) {
-            getSimulator()
-                .modelMessage("Internal transition from sendYawAngleLeft");
-
-            //ID:TRA:sendYawAngleLeft
-            holdIn("sendYawAngleRight", 0.0);
-
-            //ENDID
-            return;
-        }
-        if (phaseIs("sendYawAngleRight")) {
-            getSimulator()
-                .modelMessage("Internal transition from sendYawAngleRight");
-
-            //ID:TRA:sendYawAngleRight
-            passivateIn("passive");
-
-            //ENDID
-            return;
-        }
-
-        //passivate();
-    }
-
-    @Override
-    public void externalTransition(double timeElapsed, MessageBag input) {
-        currentTime += timeElapsed;
-        // Subtract time remaining until next internal transition (no effect if sigma == Infinity)
-        sigma -= timeElapsed;
-
-        // Store prior data
-        previousPhase = phase;
-        previousSigma = sigma;
-
-        // Fire state transition functions
-        if (phaseIs("waitforAngleLeft")) {
-            if (input.hasMessages(inAngleLeft)) {
-                ArrayList<Message<Serializable>> messageList =
-                    inAngleLeft.getMessages(input);
-
-                passivateIn("waitforAngleRight");
-
-                return;
-            }
-        }
-
-        if (phaseIs("waitforAngleRight")) {
-            if (input.hasMessages(inAngleRight)) {
-                ArrayList<Message<Serializable>> messageList =
-                    inAngleRight.getMessages(input);
-
-                holdIn("sendYawAngleLeft", 0.0);
-
-                return;
-            }
-        }
-    }
-
-    @Override
-    public void confluentTransition(MessageBag input) {
-        // confluentTransition with internalTransition first (by default)
-        internalTransition();
-        externalTransition(0, input);
-    }
-
-    @Override
-    public Double getTimeAdvance() {
-        return sigma;
-    }
-
-    @Override
-    public MessageBag getOutput() {
-        MessageBag output = new MessageBagImpl();
-
-        if (phaseIs("sendYawAngleLeft")) {
-            output.add(outYawAngleLeft, null);
-        }
-        if (phaseIs("sendYawAngleRight")) {
-            output.add(outYawAngleRight, null);
-        }
-        return output;
-    }
-
-    // Custom function definitions
-
-    // End custom function definitions
-    public static void main(String[] args) {
-        SimulationOptionsImpl options = new SimulationOptionsImpl(args, true);
-
-        // Uncomment the following line to disable SimViewer for this model
-        // options.setDisableViewer(true);
-
-        // Uncomment the following line to disable plotting for this model
-        // options.setDisablePlotting(true);
-        Wings model = new Wings();
-        model.options = options;
-
-        if (options.isDisableViewer()) { // Command line output only
-            Simulation sim =
-                new SimulationImpl("Wings Simulation", model, options);
-            sim.startSimulation(0);
-            sim.simulateIterations(Long.MAX_VALUE);
-        } else { // Use SimViewer
-            SimViewer viewer = new SimViewer();
-            viewer.open(model, options);
-        }
-    }
-
-    public void addPropertyChangeListener(String propertyName,
-        PropertyChangeListener listener) {
-        propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-        propertyChangeSupport.removePropertyChangeListener(listener);
-    }
-
-    // State variables
-    public String[] getStateVariableNames() {
-        return new String[] {  };
-    }
-
-    public Object[] getStateVariableValues() {
-        return new Object[] {  };
-    }
-
-    public Class<?>[] getStateVariableTypes() {
-        return new Class<?>[] {  };
-    }
-
-    public void setStateVariableValue(int index, Object value) {
-        switch (index) {
-
-            default:
-                return;
-        }
-    }
-
-    // Convenience functions
-    protected void passivate() {
-        passivateIn("passive");
-    }
-
-    protected void passivateIn(String phase) {
-        holdIn(phase, Double.POSITIVE_INFINITY);
-    }
-
-    protected void holdIn(String phase, Double sigma) {
-        this.phase = phase;
-        this.sigma = sigma;
-        getSimulator()
-            .modelMessage("Holding in phase " + phase + " for time " + sigma);
-    }
-
-    protected static File getModelsDirectory() {
-        URI dirUri;
-        File dir;
-        try {
-            dirUri = Wings.class.getResource(".").toURI();
-            dir = new File(dirUri);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            throw new RuntimeException(
-                "Could not find Models directory. Invalid model URL: " +
-                Wings.class.getResource(".").toString());
-        }
-        boolean foundModels = false;
-        while (dir != null && dir.getParentFile() != null) {
-            if (dir.getName().equalsIgnoreCase("java") &&
-                  dir.getParentFile().getName().equalsIgnoreCase("models")) {
-                return dir.getParentFile();
-            }
-            dir = dir.getParentFile();
-        }
-        throw new RuntimeException(
-            "Could not find Models directory from model path: " +
-            dirUri.toASCIIString());
-    }
-
-    protected static File getDataFile(String fileName) {
-        return getDataFile(fileName, "txt");
-    }
-
-    protected static File getDataFile(String fileName, String directoryName) {
-        File modelDir = getModelsDirectory();
-        File dir = new File(modelDir, directoryName);
-        if (dir == null) {
-            throw new RuntimeException("Could not find '" + directoryName +
-                "' directory from model path: " + modelDir.getAbsolutePath());
-        }
-        File dataFile = new File(dir, fileName);
-        if (dataFile == null) {
-            throw new RuntimeException("Could not find '" + fileName +
-                "' file in directory: " + dir.getAbsolutePath());
-        }
-        return dataFile;
-    }
-
-    protected void msg(String msg) {
-        getSimulator().modelMessage(msg);
-    }
-
-    // Phase display
-    public boolean phaseIs(String phase) {
-        return this.phase.equals(phase);
-    }
-
-    public String getPhase() {
-        return phase;
-    }
-
-    public String[] getPhaseNames() {
-        return new String[] {
-            "waitforAngleLeft", "waitforAngleRight", "sendYawAngleLeft",
-            "sendYawAngleRight", "passive"
-        };
-    }
+	public final Port<? extends Serializable> inAngleLeft= addInputPort("inAngleLeft",Serializable.class);
+	public final Port<? extends Serializable> inAngleRight= addInputPort("inAngleRight",Serializable.class);
+	public final Port<? extends Serializable> outYawAngleLeft= addOutputPort("outYawAngleLeft",Serializable.class);
+	public final Port<? extends Serializable> outYawAngleRight= addOutputPort("outYawAngleRight",Serializable.class);
+	public Wings(){
+		this("Wings");
+	}
+	public Wings(String nm) {
+		super(nm);
+	}
+	public String getTooltip() {
+		return null;
+	}
+	public String[] getStateVariableNames() {
+		return  new String[]{};
+	}
+	public Object[] getStateVariableValues() {
+		return null;
+	}
+	public Class<?>[] getStateVariableTypes() {
+		return new Class<?>[0];
+	}
+	public void setStateVariableValue(int index, Object value) {}
+	public String[] getPhaseNames() {
+		return  new String[]{};
+	}
+	public String getPhase() {
+		return null;
+	}
+	public boolean phaseIs(String phase) {
+		return false;
+	}
 }
